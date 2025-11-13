ea0227ae04bd5dcf471495e561254cbe7cc416e0
gabrielfernando01 <fernando.rozepeda@gmail.com>
2025-11-13 15:27:14 -0600

    upgrade nvim lspconfig.lua

--- lua/core/plugins/lsp/lspconfig.lua
+++ lua/core/plugins/lsp/lspconfig.lua
@@ -1,22 +1,19 @@
-local lspconfig_status, lspconfig = pcall(require, "lspconfig")
+-- Updated Neovim LSP configuration using `vim.lsp.config`
+-- Fully modern and warnings-free
 
-if not lspconfig_status then
-    return
-end
 
 local cmp_nvim_lsp_status, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status then
-    return
+return
 end
 
---local typescript_setup, typescript = pcall(require, "typescript")
---if not typescript_setup then
-    --return
---end
 
 local keymap = vim.keymap
-local on_attach = function(client, buffnr)
+
+
+local on_attach = function(client, bufnr)
 local opts = { noremap = true, silent = true, buffer = bufnr }
+
 
 keymap.set("n", "gf", "<cmd>Lspsaga lsp_finder<CR>", opts)
 keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", opts)
@@ -25,73 +22,36 @@ keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<CR>", opts)
 keymap.set("n", "<leader>ca", "<cmd>Lspsaga code_action<CR>", opts)
 keymap.set("n", "<leader>rn", "<cmd>Lspsaga rename<CR>", opts)
 keymap.set("n", "<leader>d", "<cmd>Lspsaga show_line_diagnostics<CR>", opts)
-keymap.set("n", "<leader>d", "<cmd>Lspsaga show_cursor_diagnostics<CR>", opts)
 keymap.set("n", "[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", opts)
 keymap.set("n", "]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", opts)
 keymap.set("n", "K", "<cmd>Lspsaga hover_doc<CR>", opts)
-keymap.set("n", "K", "<leader>LSoutlineToggle<CR>", opts)
+
 
 if client.name == "tsserver" then
-    keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>")
+keymap.set("n", "<leader>rf", ":TypescriptRenameFile<CR>")
+end
 end
 
+
+local capabilities = cmp_nvim_lsp.default_capabilities()
+
+
+-- Modern LSP setup
+local function setup_server(name, config)
+vim.lsp.config(name, config)
 end
 
-local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-lspconfig.html.setup({
-    capabilities = capabilities,
-    on_attach = on_attach
-})
+local base = {
+capabilities = capabilities,
+on_attach = on_attach,
+}
 
-lspconfig.tailwindcss.setup({
-    capabilities = capabilities,
-    on_attach = on_attach
-})
 
-lspconfig.gopls.setup({
-    capabilities = capabilities,
-    on_attach = on_attach
-})
-
-lspconfig.rust_analyzer.setup({
-    server = {
-        capabilities = capabilities,
-        on_attach = on_attach
-    }
-})
-
---lspconfig.jdtls.setup({
---    server = {
---        capabilities = capabilities,
---        on_attach = on_attach
---    }
--- })
-
-lspconfig.harper_ls.setup({
-    server = {
-        capabilities = capabilities,
-        on_attach = on_attach
-    }
-})
-
---lspconfig.java_language_server.setup({
---    server = {
---        capabilities = capabilities,
---        on_attach = on_attach
---    }
---})
-
-lspconfig.pyright.setup({
-    server = {
-        capabilities = capabilities,
-        on_attach = on_attach
-    }
-})
-
-lspconfig.jsonls.setup({
-    server = {
-        capabilities = capabilities,
-        on_attach = on_attach
-    }
-})
+setup_server("html", base)
+setup_server("tailwindcss", base)
+setup_server("gopls", base)
+setup_server("rust_analyzer", base)
+setup_server("harper_ls", base)
+setup_server("pyright", base)
+setup_server("jsonls", base)
